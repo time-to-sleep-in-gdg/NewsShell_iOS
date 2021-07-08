f4a68abad4bbb68491a7d38e5ea78d2e412d70ba
kimxwan0319 <kimxwan0319@naver.com>
2021-07-08 23:22:04 +0900

    ⚙️ :: Tuist 세팅

---
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -0,0 +1,58 @@
+import ProjectDescription
+
+/// Project helpers are functions that simplify the way you define your project.
+/// Share code to create targets, settings, dependencies,
+/// Create your own conventions, e.g: a func that makes sure all shared targets are "static frameworks"
+/// See https://tuist.io/docs/usage/helpers/
+
+extension Project {
+    /// Helper function to create the Project for this ExampleApp
+    public static func app(name: String, platform: Platform, additionalTargets: [String]) -> Project {
+        let targets = makeAppTargets(name: name,
+                                     platform: platform,
+                                     dependencies: additionalTargets.map { TargetDependency.target(name: $0) })
+        return Project(name: name,
+                       organizationName: "com.timeToSleep",
+                       targets: targets)
+    }
+
+    // MARK: - Private
+
+    /// Helper function to create the application target and the unit test target.
+    private static func makeAppTargets(name: String, platform: Platform, dependencies: [TargetDependency]) -> [Target] {
+        let platform: Platform = platform
+        let infoPlist: [String: InfoPlist.Value] = [
+            "CFBundleShortVersionString": "1.0",
+            "CFBundleVersion": "1",
+            "UIMainStoryboardFile": "",
+            "UILaunchStoryboardName": "LaunchScreen"
+            ]
+
+        let mainTarget = Target(
+            name: name,
+            platform: platform,
+            product: .app,
+            bundleId: "com.timeToSleep.\(name)",
+            deploymentTarget: .iOS(targetVersion: "13.0", devices: .iphone),
+            infoPlist: .extendingDefault(with: infoPlist),
+            sources: ["Sources/**"],
+            resources: ["Resources/**"],
+            dependencies: dependencies + [
+                .cocoapods(path: ".")
+            ]
+        )
+        
+        let testTarget = Target(
+            name: "\(name)Tests",
+            platform: platform,
+            product: .unitTests,
+            bundleId: "com.timeToSleep.\(name)Tests",
+            infoPlist: .default,
+            sources: ["Tests/**"],
+            dependencies: [
+                .target(name: "\(name)"),
+                .cocoapods(path: ".")
+        ])
+        return [mainTarget, testTarget]
+    }
+}
