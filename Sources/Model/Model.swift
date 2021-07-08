74dfdfe1c26bc32b2baf50aeb8173228e23dae9c
kimxwan0319 <kimxwan0319@naver.com>
2021-07-08 23:41:23 +0900

    📑 :: MVVM 파일 구조 적립

---
+++ Sources/Model/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  NewsShell
+//
+//  Created by 김수완 on 2021/07/08.
+//  Copyright © 2021 com.timeToSleep. All rights reserved.
+//
+
+import Foundation
---
+++ Sources/View/ViewController/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  NewsShell
+//
+//  Created by 김수완 on 2021/07/08.
+//  Copyright © 2021 com.timeToSleep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
