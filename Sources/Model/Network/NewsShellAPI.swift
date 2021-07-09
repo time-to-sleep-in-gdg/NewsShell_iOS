c5eb98649134175ebf3c50e67400295f3755c69c
kimxwan0319 <kimxwan0319@naver.com>
2021-07-09 10:35:16 +0900

    ⚡️ :: NewsShellAPI 템플릿 작성

--- Sources/Model/Network/NewsShellAPI.swift
+++ Sources/Model/Network/NewsShellAPI.swift
@@ -7,3 +7,57 @@
 //
 
 import Foundation
+import Alamofire
+
+enum NewsShellAPI {
+    
+}
+
+extension NewsShellAPI {
+
+    public var uri: String {
+        let baseUrl = "http://13.125.161.204:8080"
+        return baseUrl + path
+    }
+
+    public var method: HTTPMethod {
+        switch self {
+        default:
+            return .get
+        }
+    }
+
+    public var parameters: Parameters? {
+        switch self {
+        default:
+            return nil
+        }
+    }
+
+    public var encoding: ParameterEncoding {
+        switch self.method {
+        case .get:
+            return URLEncoding.queryString
+        default:
+            return JSONEncoding.default
+        }
+    }
+
+    public var header: HTTPHeaders? {
+        switch self {
+        default:
+            return nil
+        }
+    }
+
+    private var path: String {
+        switch self {
+        default:
+            return ""
+        }
+    }
+
+    private func encodingQuery(query: String) -> String {
+        return query.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed) ?? query
+    }
+}
